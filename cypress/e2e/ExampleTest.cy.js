525f6cad29156538ef968ca65e22b858ea25b762
Evan <memoryevan@gmail.com>
2023-08-26 09:27:45 -0400

    Initial commit

---
+++ cypress/e2e/ExampleTest.cy.js
@@ -0,0 +1,26 @@
+describe('suite name', () => {
+
+    it('test1', () => {
+      expect(true).to.equal(true)
+    })
+
+    it('test2', () => {
+        expect(true).to.equal(true)
+      })
+
+      it('test3', () => {
+        expect(true).to.equal(true)
+      })
+  }
+  )
+
+describe('suite name', function()
+{
+
+    it('test1', function()
+    {
+        expect(true).to.equal(true)
+    }
+    )
+}
+)
